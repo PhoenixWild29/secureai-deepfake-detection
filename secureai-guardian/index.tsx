baf64353439e28cf85f63ab717107460bafda316
PhoenixWild29 <phoenixwild29@github.com>
2026-01-17 10:52:49 -0500

    Fix: Remove Tailwind CDN, add favicon, fix chart dimensions

--- secureai-guardian/index.tsx
+++ secureai-guardian/index.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import ErrorBoundary from './components/ErrorBoundary';
+import './index.css';
 
 // Debug logging
 console.log('🚀 SecureAI Guardian - Starting React app...');
